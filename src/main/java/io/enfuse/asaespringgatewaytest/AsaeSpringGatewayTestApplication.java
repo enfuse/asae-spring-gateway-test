6dc0eae2b97fb8f728786796d72ceefa6cb713d8
RJ Small <rjsmall@Enfuseios-MBP.askey.com>
2022-09-30 14:49:13 -0400

    initial commit

---
+++ src/main/java/io/enfuse/asaespringgatewaytest/AsaeSpringGatewayTestApplication.java
@@ -0,0 +1,13 @@
+package io.enfuse.asaespringgatewaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsaeSpringGatewayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsaeSpringGatewayTestApplication.class, args);
+	}
+
+}
